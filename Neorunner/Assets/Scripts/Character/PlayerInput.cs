2a1e81600ae07dd3fe4c54c62d7c2fdff32905dc
bryanmiranda01 <bryanmiranda13579@gmail.com>
2023-10-12 17:37:55 -0500

    Movement/Sprite updates

--- Neorunner/Assets/Scripts/Character/PlayerInput.cs
+++ Neorunner/Assets/Scripts/Character/PlayerInput.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     DiscreteMovement discretemovement;
+    public Rigidbody2D rb;
+    public float jumpAmount = 35;
+    public float gravityScale = 10;
+    public float fallingGravityScale = 40;
 
     void Awake () {
         discretemovement = GetComponent<DiscreteMovement>();
@@ -12,6 +16,7 @@ public class PlayerInput : MonoBehaviour
 
     void Update () {
         
+        //Movement
         Vector3 vel = Vector3.zero;
 
         if(Input.GetKey(KeyCode.A)) {
@@ -20,14 +25,23 @@ public class PlayerInput : MonoBehaviour
         else if(Input.GetKey(KeyCode.D)) {
             vel.x = 1;
         }
-        else if(Input.GetKey(KeyCode.W)) {
-            vel.y = 1;
-        }
         else if(Input.GetKey(KeyCode.S)) {
             vel.y = -1;
         }
-
         discretemovement.MoveTransform(vel);
-    }
 
+        //Jump
+         if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        }
+        if(rb.velocity.y >= 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallingGravityScale;
+        }
+    }
 }
