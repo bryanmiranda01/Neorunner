112e997cecb5d07a615b2fb87dc0f1e8bc479916
bryanmiranda01 <bryanmiranda13579@gmail.com>
2023-10-31 17:15:49 -0500

    Enemy shooting updates

--- Neorunner/Assets/Scripts/Character/Bullet.cs
+++ Neorunner/Assets/Scripts/Character/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
-    private float timer;
+    
     void Start()
     {
         rb.velocity = transform.right * speed;
